efba699248d94f9d33ffeb436f40471d610b595f
jacobwojcik <jkbwojcik11@gmail.com>
2023-08-02 23:01:21 +0200

    Specify font subset

--- app/z-optymalizacja/layout.tsx
+++ app/z-optymalizacja/layout.tsx
@@ -1,6 +1,6 @@
 import { Roboto } from 'next/font/google';
 
-const font = Roboto({ weight: '400' });
+const font = Roboto({ weight: '400', subsets: ['latin'] });
 
 export default function Layout({ children }: { children: React.ReactNode }) {
   return (
