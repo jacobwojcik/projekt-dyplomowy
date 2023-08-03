47dff92704004814b628dea834113490b11d00d1
jacobwojcik <jkbwojcik11@gmail.com>
2023-08-03 20:49:56 +0200

    Update unoptimized page

--- app/bez-optymalizacji/page.tsx
+++ app/bez-optymalizacji/page.tsx
@@ -1,32 +1,20 @@
 import './style.css';
 
 import ProductCard from '@/components/ProductCard';
-import {
-  Card,
-  CardContent,
-  CardFooter,
-  CardHeader,
-  CardTitle,
-} from '@/components/ui/Card';
 import { getProducts } from '@/lib/queries';
 
 export default async function Page() {
   const products = await getProducts();
 
   return (
-    <main className="mx-auto my-6 flex max-w-7xl flex-col gap-8 md:grid md:grid-cols-3">
-      <div className="flex flex-col gap-6 md:col-span-1">
-        <Card className="w-full text-center">
-          <CardHeader className="w-full">
-            <CardTitle>Bez optymalizacji</CardTitle>
-          </CardHeader>
-          <CardContent className="justify-content flex flex-col items-center gap-6">
-            <img
-              src="https://edceah5uf5z.exactdn.com/wp-content/uploads/2023/02/kot.jpg?strip=all&lossy=1&sharp=1&ssl=1"
-              alt="Image 1"
-              width={360}
-            ></img>
-            <span>
+    <main className="mx-auto my-6 max-w-7xl">
+      <div className="mx-4 flex flex-col gap-6 md:col-span-1">
+        <div className="grid w-full grid-cols-1 rounded-lg border border-zinc-200 p-4 text-center md:grid-cols-3">
+          <div className="col-span-1">
+            <h1 className="font-roboto mb-2 text-2xl font-semibold">
+              Bez optymalizacji
+            </h1>
+            <span className="font-roboto">
               Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer
               nulla justo, consectetur sit amet dui ac, ultrices viverra tortor.
               Duis eget rhoncus nulla. Morbi vel nibh et ex sagittis ultrices.
@@ -38,14 +26,21 @@ export default async function Page() {
               rutrum, est magna ultricies eros, quis maximus nibh nulla gravida
               nibh.
             </span>
-          </CardContent>
-          <CardFooter></CardFooter>
-        </Card>
+          </div>
+          <div className="col-span-2 mx-auto flex w-2/3 flex-col items-center justify-center gap-6">
+            <img
+              className="rounded-lg"
+              src="https://mangools.com/blog/wp-content/uploads/2019/07/learn-seo-new-t.png"
+              alt="Image 1"
+              width={560}
+            ></img>
+          </div>
+        </div>
       </div>
 
-      <div className="w-full md:col-span-2">
-        <h1 className="text-lg font-semibold">Najnowsze okazje!</h1>
-        <div className="grid-col-1 my-4 grid gap-6 sm:grid-cols-2 lg:grid-cols-3">
+      <div className="mx-4 w-full md:col-span-2">
+        <h1 className="my-4 text-lg font-semibold">Najnowsze okazje!</h1>
+        <div className="grid-col-1 my-4 grid gap-6 sm:grid-cols-2 lg:grid-cols-4">
           {products.map((product) => (
             <ProductCard key={product.id} product={product} />
           ))}
