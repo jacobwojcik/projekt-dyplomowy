e9fb1221f9b72daf600baba55f92c487f6ec5ccd
jacobwojcik <jkbwojcik11@gmail.com>
2023-08-17 20:50:52 +0200

    Update revalidation page

--- app/rewalidacja/page.tsx
+++ app/rewalidacja/page.tsx
@@ -1,11 +1,32 @@
-import AddProductForm from '@/components/AddProductForm';
-import { getCategories } from '@/lib/queries';
+import AddTodoForm from '@/components/AddTodoForm';
+import TodosList from '@/components/TodosList';
+import {
+  Sheet,
+  SheetContent,
+  SheetHeader,
+  SheetTitle,
+  SheetTrigger,
+} from '@/components/ui/Sheet';
+import { getCategories, getTodos } from '@/lib/queries';
 
 export default async function Page() {
   const categories = await getCategories();
+
+  const todos = await getTodos();
   return (
-    <div className="flex h-screen w-full items-center justify-center">
-      <AddProductForm options={categories} />
+    <div className="mx-4 my-6 flex h-screen w-full flex-col items-center justify-center">
+      <TodosList todos={todos} />
+      <Sheet>
+        <SheetTrigger className="my-4 rounded-lg border border-black bg-white px-3 py-2 text-center text-black hover:opacity-75">
+          Dodaj nowe zadanie
+        </SheetTrigger>
+        <SheetContent>
+          <SheetHeader>
+            <SheetTitle>Dodaj nowe zadanie</SheetTitle>
+          </SheetHeader>
+          <AddTodoForm options={categories} />
+        </SheetContent>
+      </Sheet>
     </div>
   );
 }
