aad708a945f9a4c9cf4af18d6ca6f33ed666871c
jacobwojcik <jkbwojcik11@gmail.com>
2023-08-19 13:35:13 +0200

    Fix page height

--- app/rewalidacja/page.tsx
+++ app/rewalidacja/page.tsx
@@ -8,7 +8,7 @@ export const revalidate = 300;
 export default async function Page() {
   const todos = await getTodos();
   return (
-    <div className="mx-auto my-6 flex h-screen max-w-7xl flex-col items-center">
+    <div className="mx-auto my-6 flex max-w-7xl flex-col items-center">
       <div className="flex w-full flex-col px-4 ">
         <div className="flex justify-between gap-6">
           <h1 className="my-6 text-2xl font-semibold">Lista zadań</h1>
