9cf234b2f5e032891befda1046109ee36d137f9b
jacobwojcik <jkbwojcik11@gmail.com>
2023-10-11 23:04:38 +0200

    Update optimization

--- app/z-optymalizacja/layout.tsx
+++ app/z-optymalizacja/layout.tsx
@@ -1,5 +1,4 @@
 import { Roboto_Mono } from 'next/font/google';
-import Script from 'next/script';
 
 const robotoFont = Roboto_Mono({
   subsets: ['latin'],
@@ -12,18 +11,6 @@ export default function Layout({ children }: { children: React.ReactNode }) {
   return (
     <>
       <main className={robotoFont.className}>{children}</main>
-      <Script
-        strategy="afterInteractive"
-        dangerouslySetInnerHTML={{
-          __html: `
-              const end = Date.now() + 1000;  
-              while (Date.now() < end) {
-                  // Block thread for 1 second
-              }
-              console.log('Zewnętrzny skrypt został zakończony!');
-              `,
-        }}
-      ></Script>
     </>
   );
 }
--- components/features/optimization/UnoptimizedHero.tsx
+++ components/features/optimization/UnoptimizedHero.tsx
@@ -16,8 +16,8 @@ const UnoptimizedHero = () => {
   return (
     <div className="mx-4 flex flex-col gap-6 md:col-span-1">
       <div className="grid w-full grid-cols-1 rounded-lg border border-zinc-200 p-4 text-center md:grid-cols-3">
-        <div className="col-span-1">
-          <h1 className="mb-2 text-2xl font-semibold">Bez optymalizacji</h1>
+        <div className="col-span-1 flex flex-col justify-center">
+          <h1 className="mb-2 text-2xl font-semibold">Czego nie robić?</h1>
           <span className="text-xs sm:text-base">
             Aktualna strona jest przykładem tego czego{' '}
             <span className="font-semibold">NIE</span> robić. Quisque at lectus
@@ -28,7 +28,7 @@ const UnoptimizedHero = () => {
         </div>
         <div className="col-span-2 mx-auto flex w-full sm:w-2/3 flex-col items-center justify-center gap-6 h-[400px] sm:h-auto">
           {/* eslint-disable-next-line jsx-a11y/alt-text */}
-          <img className="rounded-lg md:h-[200px]" src={imgSrc}></img>
+          <img className="rounded-lg md:h-[260px] h-[320px]" src={imgSrc}></img>
         </div>
       </div>
     </div>
