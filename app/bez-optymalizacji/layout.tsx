13c1a8f308edfc478eaa7aa1454fa7e2f1aedc69
jacobwojcik <jkbwojcik11@gmail.com>
2023-10-11 22:22:18 +0200

    Add heavy script to unoptimized page

--- app/bez-optymalizacji/layout.tsx
+++ app/bez-optymalizacji/layout.tsx
@@ -1,3 +1,16 @@
 export default function Layout({ children }: { children: React.ReactNode }) {
-  return <>{children}</>;
+  return (
+    <>
+      {children}
+      <script
+        dangerouslySetInnerHTML={{
+          __html: `
+              setTimeout(() => {
+                console.log('Zewnętrzny skrypt został zakończony!');
+              }, 1000);
+              `,
+        }}
+      ></script>
+    </>
+  );
 }
