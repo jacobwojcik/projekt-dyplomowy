ab6065626b2c97e024a5599f2663799701dab3d6
jacobwojcik <jkbwojcik11@gmail.com>
2023-12-08 17:34:34 +0100

    Fix products filtering

--- lib/queries/getProducts.ts
+++ lib/queries/getProducts.ts
@@ -1,22 +1,28 @@
 import type { ProductInfo } from '@/types';
 import { supabase } from '../utils';
 
-
-
 export const getProducts = async (
   limit = 8,
   from = 0,
-  sort?: string, 
+  sort?: string,
   category?: string
 ): Promise<ProductInfo[]> => {
-  
-  const { data: products } =  await supabase
+  const query = supabase
     .from('product')
     .select('*, category!inner(name)')
     .range(from, from + limit - 1)
-    .order('price', { ascending: sort === 'asc' })
-    .eq('category.name', category || '') 
+
     .returns<ProductInfo[]>();
-  
-    return products ?? [];
+
+  if (category) {
+    // @ts-ignore
+    query.eq('category.name', category);
+  }
+
+  if (sort) {
+    query.order('price', { ascending: sort === 'asc' });
+  }
+
+  const { data: products } = await query.returns<ProductInfo[]>();
+  return products ?? [];
 };
